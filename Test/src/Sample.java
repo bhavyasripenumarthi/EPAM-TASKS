5333634be48ca19de445fb562c1fbad4d7d18c16
welcome <welcome@LAPTOP-KBJL1CDK>
2019-03-19 23:20:46 +0530

    Sample Java Project

---
+++ Test/src/Sample.java
@@ -0,0 +1,9 @@
+
+public class Sample {
+	/*
+	 * Sample Hello World Program
+	 */
+	public static void main(String args[]) {
+		System.out.println("Hello World!!!");
+	}
+}
---
+++ Test/src/TestSample.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestSample {
+	/*
+	 * Test Sample Project
+	 */
+	@Test
+	public void test() {
+		assert(true);
+	}
+
+}
